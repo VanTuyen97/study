3325ab063a32f9be41acd48b4265f6f699c8090d
TuyenNV22 <TuyenNV22@TEMP-TUYENNV22.fsoft.fpt.vn>
2018-09-17 17:16:44 +0700

    Rest Services Spring boot

--- HelloWorldSpringBoot/src/main/java/com/vantuyen361/helloworldspringboot/HelloWorld/HelloWorld.java
+++ HelloWorldSpringBoot/src/main/java/com/vantuyen361/helloworldspringboot/HelloWorld/HelloWorld.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.vantuyen361.helloworldspringboot;
+package com.vantuyen361.helloworldspringboot.HelloWorld;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
@@ -14,15 +14,15 @@ import org.springframework.web.bind.annotation.RestController;
  *
  * @author Tuyen
  */
-@RestController
-@EnableAutoConfiguration
-public class Main {
+@RestController//A convenience annotation that is itself annotated with @Controller and @ResponseBody.
+@EnableAutoConfiguration//Auto-configuration classes are usually applied based on your classpath and what beans you have defined
+public class HelloWorld {
     @RequestMapping("/")
     String home(){
         return "Hello World";
     }
     
     public static void main(String []args)throws Exception{
-        SpringApplication.run(Main.class, args);
+        SpringApplication.run(HelloWorld.class, args);
     }
 }
